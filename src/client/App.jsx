453ccd4d19bc41a0fac645e11d458d44d5400602
ReadyHash <Readyhash@gmail.com>
2020-06-02 10:06:15 +0800

    Attemped MVP

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
+class Form extends React.Component {
+    constructor(){
+      super()
+
+      this.state = {
+        word : "",
+        list: ""
+      }
+    }
+
+    changeHandler(event){
+      this.setState({word:event.target.value});
+      console.log("change", event.target.value);
+    }
+
+
+
+    render() {
+        var createTask = () => {
+            console.log("a task is created!");
+            this.setState({list:event.target.value})
+            console.log(this.state.list);
+        }
+
+        console.log("rendering");
+        return (
+          <div>
+
+            <p>{this.state.word}</p>
+            <input onChange={(event)=>{this.changeHandler(event);}}/>
+            <button onClick={createTask}>Create Task</button>
+          </div>
+        );
+    }
+}
+
+
 class App extends React.Component {
   render() {
     return (
       <div>
         Welcome.
+        <Form/>
       </div>
     );
   }
